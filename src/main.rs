9c5437fffc559b07f3a90dc12cc0aa2b2faf419b
StanaAndrei <stadey33@gmail.com>
2025-03-06 21:52:07 +0200

    generalised img modify

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 
     let input_path = matches.value_of("input_file").unwrap();
     let output_path = file::add_suffix_to_filename(input_path, "changed");
-    img::modify_image_slightly(input_path, output_path.as_str());
+    img::modify_1st_pixel_red_ch(input_path, output_path.as_str(), 1);
 
     let original_hash = sha::get_file_hash(input_path);
     println!("Original file SHA256: {}", original_hash);
--- src/utils/img.rs
+++ src/utils/img.rs
@@ -1,10 +1,13 @@
-pub fn modify_image_slightly(input_path: &str, output_path: &str) {
+pub fn modify_1st_pixel_red_ch(input_path: &str, output_path: &str, inc: i8) {
     let img = image::open(input_path).expect("Failed to open image");
     let mut modified_img = img.to_rgba8();
     let pixel = modified_img.get_pixel_mut(0, 0);
-    // Modify the pixel slightly (adjust the red channel by 1)
-    // This change is imperceptible to humans
-    pixel[0] = pixel[0].saturating_add(1);
+    let abs: u8 = inc.unsigned_abs();
+    if inc > 0 {
+        pixel[0] = pixel[0].saturating_add(abs);
+    } else {
+        pixel[0] = pixel[0].saturating_sub(abs);
+    }
     modified_img.save(output_path).expect("Failed to save modified image");
     println!("Image modified and saved to {}", output_path);
 }
